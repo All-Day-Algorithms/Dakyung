fa8c47d2e327402053d3af3e2f944eccbc8b4033
Dakyung1 <dakyung1390@gmail.com>
2019-01-12 11:24:49 +0900

    first

--- src/num10998.java
+++ src/num10998.java
@@ -1,4 +1,13 @@
-
+import java.util.*;
 public class num10998 {
-
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		
+		int num1 =scan.nextInt();
+		int num2 = scan.nextInt();
+		scan.close();
+		
+		System.out.println(num1 * num2);
+		
+	}
 }
