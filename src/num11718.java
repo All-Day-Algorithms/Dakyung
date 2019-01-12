b0e7b4f450fd2f0298648e988b5701f9ab46bffc
Dakyung1 <dakyung1390@gmail.com>
2019-01-12 11:07:31 +0900

    first

--- src/num11718.java
+++ src/num11718.java
@@ -1,4 +1,18 @@
+import java.util.*;
 
 public class num11718 {
-
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextLine()) {
+			String inputs = scan.nextLine();
+			
+			if(inputs.length()>100 || inputs.isEmpty()||inputs.startsWith(" ")||inputs.endsWith(" ")){
+				break;
+			}
+			System.out.println(inputs);
+		}
+		scan.close();
+	}
+	
 }
