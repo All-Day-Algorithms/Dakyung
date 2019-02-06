9d6bb68fbb4cc21f430c12e8c83730d7d1c2aa44
Dakyung1 <dakyung1390@gmail.com>
2019-02-06 22:30:01 +0900

    first

---
+++ src/num11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class num11654 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        
+        int c = scan.next().charAt(0);
+        System.out.println((int)c);
+        
+        scan.close();
+
+	}
+
+}
